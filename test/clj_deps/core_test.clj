18d2a34bd49b40ab8e8446f0ea8ac9fa5bc776e1
Ilshat Sultanov <just.sultanov@gmail.com>
2020-01-17 10:12:05 +0300

    fixed test

--- test/clj_deps/core_test.clj
+++ test/clj_deps/core_test.clj
@@ -5,11 +5,8 @@
 
 (deftest test-add-lib
   (testing "should be added clojure library from git by https"
-    (is (sut/add-lib 'clj-helpers {:git/url "https://github.com/just-sultanov/clj-helpers.git"
-                                   :sha     "db6263b750a66c3f3df99e14a53ba9e416f34fa3"}))
     (is (sut/add-lib 'clj-fsm {:git/url "https://github.com/just-sultanov/clj-fsm.git"
                                :sha     "396f95a1f28a1fae7672bdf4793e9168abaf2990"})))
 
   (testing "should be added clojure library from maven"
-    (is (true? (sut/add-lib 'clj-helpers {:mvn/version "0.2.0"})))
     (is (true? (sut/add-lib 'clj-fsm {:mvn/version "0.2.2"})))))
